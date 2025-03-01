0d61b3df601056be0932ab09ac2f3e44841a6ef5
Merezha Roman <rmerezha@gmail.com>
2025-03-01 18:47:59 +0200

    Add func mathutils.SolveQuadratic

---
+++ mathutils/quadratic.go
@@ -0,0 +1,18 @@
+package mathutils
+
+import "math"
+
+func SolveQuadratic(a, b, c float64) (roots []float64, hasSolution bool) {
+	D := b*b - 4*a*c
+	if D < 0 {
+		return nil, false
+	}
+	if D == 0 {
+		return []float64{-b / (2 * a)}, true
+	}
+	sqrtD := math.Sqrt(D)
+	return []float64{
+		(-b + sqrtD) / (2 * a),
+		(-b - sqrtD) / (2 * a),
+	}, true 
+}
